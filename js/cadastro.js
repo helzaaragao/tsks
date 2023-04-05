a5b077384bf45a736ab0ba27230ed996c0e161fe
helzaaragao <helza.batista.aragao@gmail.com>
2023-04-05 11:45:37 -0300

    bugs okay

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,24 +1,49 @@
-let configuracoesRequisicao = { 
-    method: 'POST', 
-    body: {
-        "firstName": "Afra",
-        "lastName": "Taiza",
-        "email": "afrataiza@test.com",
-        "password": "12345678"
-      }, 
-    headers:{ 
-        'Content-type': 'application/json'
-    },
-}
-        //function muxando dos pinuts esses valores
+let nomeUser = document.getElementById("name"); 
+let sobrenomeUser = document.getElementById("sobrename"); 
+let emailUser = document.getElementById("email"); 
+let senhaUser = document.getElementById("password");
+let botaoUser = document.getElementById("button");
+
+let registerUser = {
+        "firstName": nomeUser,
+        "lastName": sobrenomeUser,
+        "email": emailUser,
+        "password": senhaUser
+};
+
+nomeUser.addEventListener("input", () => {
+    registerUser.firstName = nomeUser.value
+});
+
+sobrenomeUser.addEventListener("input", () => {
+    registerUser.lastName = sobrenomeUser.value
+});
+
+emailUser.addEventListener("input", () => {
+    registerUser.email = emailUser.value
+});
+
+senhaUser.addEventListener("input", () => {
+    registerUser.password = senhaUser.value
+});
 
 async function cadastro(){ 
+    const registerUserJson = JSON.stringify(registerUser); 
+    const configuracoesRequisicao = { 
+        method: 'POST', 
+        body: registerUserJson, 
+        headers:{ 
+            'Content-type': 'application/json'
+        },
+    }
     const resposta = await fetch("https://todo-api.ctd.academy/v1/users", configuracoesRequisicao)
     let chaveJwt = await resposta.json();
     if(chaveJwt.jwt){
         window.location.href = "./index.html";
        
+    } else{ 
+        alert("Não foi possível realizar o cadastro")
     }
 }
 
-cadastro();
+botaoUser.addEventListener("click", cadastro);
--- js/entrada.js
+++ js/entrada.js
@@ -1,33 +1,52 @@
 let emailLogin = document.getElementById("email"); 
-let senhalogin = document.getElementById("password");
+let senhaLogin = document.getElementById("password");
 let botaoLogin = document.getElementById("login"); 
 let formLogin = document.getElementById("form");
 
 let loginUsuario = { 
-        "email": "afrataiza@test.com",
-        "password": "12345678"
-        //function muxando dos pinuts esses valores
-}
+    "email": emailLogin.value,
+    "password": senhaLogin
+} 
 
-const loginUsuarioJson = JSON.stringify(loginUsuario); 
+emailLogin.addEventListener("input", () => {
+    loginUsuario.email = emailLogin.value
+});
 
-let configuracoesRequisicao = { 
-    method: 'POST', 
-    body: loginUsuario, 
-    headers:{ 
-        'Content-type': 'application/json'
-    },
-}
+senhaLogin.addEventListener("input", () => {
+    loginUsuario.password = senhaLogin.value
+});
+
+/*function emailValido(emailColocado){ 
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailColocado)) {
+        return console.log(emailLogin.value);
+      } else {
+        return false;
+      }
+} */
 
 async function fazerLogin(){ 
-    const resposta = await fetch("http://todo-api.ctd.academy:3000//v1/users/login", configuracoesRequisicao)
-    let chaveJwt = await resposta.json();
-    console.log(chaveJwt);  
-    if(chaveJwt.jwt){
-        window.location.href = "./index.html";
-       
+    const loginUsuarioJson = JSON.stringify(loginUsuario); 
+   
+    const configuracoesRequisicao = { 
+        method: 'POST', 
+        body: loginUsuarioJson, 
+        headers:{ 
+            'Content-type': 'application/json'
+        },
+    }
+    const resposta = await fetch("https://todo-api.ctd.academy/v1/users/login", configuracoesRequisicao)
+    let chaveJwt = await resposta.json(); 
+     if(chaveJwt.jwt){
+     window.location.href = "./index.html";
+     } else { 
+     alert("sem as informações necessárias para logar"); 
     }
 }
 
 botaoLogin.addEventListener("click", fazerLogin);
 
+ /* Campos de Nome e Sobrenome não devem estar vazios e o campo de email deve conter um email válido para que o formulário não apresente erros 
+ deve-se desbloquear o botão de login quando os campos estão preenchidos e o campo de email está validado.
+ Fazer validação do lembra-me do usuário. Se marcado, o token do usuário deve ser salvo no local storage, se não o token deve ser salvo no session storage.
+ Um ícone de Loading enquanto ocorre o processo de login e de cadastro do usuário.
+ */
