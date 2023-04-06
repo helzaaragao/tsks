7c7322603c1fd8af1a43b72afc9e3691f15bb348
helzaaragao <helza.batista.aragao@gmail.com>
2023-04-06 15:28:48 -0300

    terminando umas coisas

--- js/entrada.js
+++ js/entrada.js
@@ -2,31 +2,55 @@ let emailLogin = document.getElementById("email");
 let senhaLogin = document.getElementById("password");
 let botaoLogin = document.getElementById("login"); 
 let formLogin = document.getElementById("form");
+let mostrarSenha = document.getElementById("show-password-checkbox");
+let lembrarLogin = document.getElementById("checkbox-remember"); 
 
 let loginUsuario = { 
     "email": emailLogin.value,
-    "password": senhaLogin
+    "password": senhaLogin.value
 } 
 
 emailLogin.addEventListener("input", () => {
     loginUsuario.email = emailLogin.value
+    valideForm();
 });
 
 senhaLogin.addEventListener("input", () => {
     loginUsuario.password = senhaLogin.value
+    valideForm();
 });
 
-/*function emailValido(emailColocado){ 
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailColocado)) {
-        return console.log(emailLogin.value);
-      } else {
-        return false;
-      }
-} */
+function validEmail(email){ 
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+function valideForm(){ 
+    const emailValida = emailLogin.value.trim(); 
+    const senhaValida = senhaLogin.value.trim(); 
+
+    if (emailValida === "" || !validEmail(emailValida)) { 
+        botaoLogin.disabled = true; 
+        ///span ativo class remove  span.innerText + remove  SEPARAR O MEIAL E SENHA  senha  < 8 
+    } else if (senhaValida === "") { 
+        botaoLogin.disabled = true;
+        //span desativo class list add 
+    } else{ 
+        botaoLogin.disabled = false; // else false no fim
+    }
+}
+
+mostrarSenha.addEventListener("change",() => { 
+    if (mostrarSenha.checked){ 
+        senhaLogin.type = "text"; 
+    } else { 
+        senhaLogin.type = "password"
+    }
+})
+
 
 async function fazerLogin(){ 
     const loginUsuarioJson = JSON.stringify(loginUsuario); 
-   
     const configuracoesRequisicao = { 
         method: 'POST', 
         body: loginUsuarioJson, 
@@ -36,17 +60,31 @@ async function fazerLogin(){
     }
     const resposta = await fetch("https://todo-api.ctd.academy/v1/users/login", configuracoesRequisicao)
     let chaveJwt = await resposta.json(); 
-     if(chaveJwt.jwt){
+     if(chaveJwt.jwt){ //ver isso aqui 
+        if(lembrarLogin.checked){ 
+          localStorage.setItem("chaveJwt", chaveJwt);
+        } else{ 
+            sessionStorage.setItem("chaveJwt", chaveJwt);
+        }  
      window.location.href = "./index.html";
      } else { 
-     alert("sem as informações necessárias para logar"); 
+       alert("Usuário ou senha inválido")
     }
 }
 
 botaoLogin.addEventListener("click", fazerLogin);
 
  /* Campos de Nome e Sobrenome não devem estar vazios e o campo de email deve conter um email válido para que o formulário não apresente erros 
- deve-se desbloquear o botão de login quando os campos estão preenchidos e o campo de email está validado.
  Fazer validação do lembra-me do usuário. Se marcado, o token do usuário deve ser salvo no local storage, se não o token deve ser salvo no session storage.
  Um ícone de Loading enquanto ocorre o processo de login e de cadastro do usuário.
+
+ Exibi a senha + Lembrar o login na próxima vez, aqueles que não foram lembrados é pra colocar na session
+
+   class disabled: oppacite  no html
+   hidden no span, colocar as classes no span , javascript hidden
+   https://daisyui.com/components/button/ 
+   setTimeout(() => {
+        console.log('Acabou o tempo')}, 3000);
+        setTimeout(() => {
+            console.log('Acabou o tempo')}, 3000);
  */
